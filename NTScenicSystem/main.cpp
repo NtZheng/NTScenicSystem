d4ae06d28a39dd17d024157a71cda1cbeb43a77f
NtZheng <2745674085@qq.com>
2016-11-18 11:06:34 +0800

    Initial Commit

---
+++ NTScenicSystem/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NTScenicSystem
+//
+//  Created by Nineteen on 11/18/16.
+//  Copyright © 2016 Nineteen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
