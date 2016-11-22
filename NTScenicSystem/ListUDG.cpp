6bd91a9e8ad2db2ea261288f17d6492c8eef2c55
NtZheng <2745674085@qq.com>
2016-11-22 12:48:00 +0800

    解决了字符编码的问题（终于正确了，唉，悲伤逆流成河）

--- NTScenicSystem/ListUDG.cpp
+++ NTScenicSystem/ListUDG.cpp
@@ -16,38 +16,29 @@
 
 void ListUDG::creatGraph() {
     auto& tempAllVertexes = *this->allVertexes;
-    // 用于读取资源
-    char tempNumber[16];
-    char tempString[128];
+
+    ifstream in("/Users/Nineteen/Documents/SourceTree/NTScenicSystem/NTScenicSystem/test.txt");
     
-    ifstream in("/Users/nineteen/Documents/SourceTree/NTScenicSystem.git/NTScenicSystem/info.txt");
+    in >> this->vertexNumber >> this->edgeNumber;
     
     // 读取顶点和边的数量
-    memset(tempNumber, '\0', 16);// 清空数组内容,达到复用效果
-    in.getline(tempNumber, 16, ' ');
-    this->vertexNumber = atoi(tempNumber);
-    
-    memset(tempNumber, '\0', 16);
-    in.getline(tempNumber, 16, '\n');
-    this->edgeNumber = atoi(tempNumber);
     
     cout << "请输入节点个数: " << this->vertexNumber << " 请输入边的条数: " << this->edgeNumber << endl;
     
     cout << "请输入节点的信息: " << endl;
     
+    
+    
     // 读取每个节点的信息
     for (unsigned i = 0; i < this->vertexNumber; i++) {
         // 分别读取一行中的三个部分
-        memset(tempString, '\0', 128);
-        in.getline(tempString, 128, ' ');
-        string name(tempString);
-        cout << name << ' ';
-        memset(tempNumber, '\0', 16);
-        in.getline(tempNumber, 16, ' ');
-        unsigned welcomeStar = atoi(tempNumber);
-        memset(tempString, '\0', 128);
-        in.getline(tempString, 128);
-        string introduction(tempString);
+        string name;
+        unsigned welcomeStar;
+        string introduction;
+        
+        in >> name >> welcomeStar >> introduction;
+        
+        cout << name << "  ";
         
         // 将这一个节点存入到allVertexes中
         tempAllVertexes[name] = make_shared<Vertex>(name, i, welcomeStar, introduction);
@@ -59,21 +50,15 @@ void ListUDG::creatGraph() {
     
     cout << "请输入边的信息: " << endl;
     
+    // 分别读取一行中的三个部分
+    string from;
+    string to;
+    int distance;
+    
     // 读取每条边的信息
     for (int i = 0; i < this->edgeNumber; i++) {
-        // 分别读取一行中的三个部分
-        memset(tempString, 128, '\0');
-        in.getline(tempString, 128, ' ');
-        string from(tempString);
-        cout << from << ' ';
-        memset(tempString, 128, '\0');
-        in.getline(tempString, 128, ' ');
-        string to(tempString);
-        cout << to << ' ';
-        memset(tempNumber, 16, '\0');
-        in.getline(tempNumber, 16);
-        int distance = atoi(tempNumber);
-        cout << distance << endl;
+        
+        in >> from >> to >> distance;
         
         // 将定点与边建立联系
         shared_ptr<Vertex> fromVertex = tempAllVertexes[from];
